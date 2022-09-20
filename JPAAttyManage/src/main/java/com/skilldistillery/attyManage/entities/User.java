f7cecb00edbc17bc24c3972e79828eb359dc7e2f
thaodev <71045132+thaodev@users.noreply.github.com>
2022-09-20 17:43:09 -0600

    Complete add sql files and update login db

--- JPAAttyManage/src/main/java/com/skilldistillery/attyManage/entities/User.java
+++ JPAAttyManage/src/main/java/com/skilldistillery/attyManage/entities/User.java
@@ -1,10 +1,40 @@
 package com.skilldistillery.attyManage.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	private String username;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + "]";
+	}
+	
+	
 
 }
